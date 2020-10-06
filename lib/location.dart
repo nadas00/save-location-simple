3986a2202a8b53d1edb46a616d887969a2878499
hasan çiftçi <se.hasancifti@gmail.com>
2020-10-06 13:32:52 +0300

    first commit

---
+++ lib/location.dart
@@ -0,0 +1,18 @@
+class Location {
+  int id;
+  String name;
+  String description;
+  double latitude;
+  double longitude;
+  DateTime lastVisit;
+  String categoryColour;
+
+  Location(
+      {this.id,
+      this.name,
+      this.description,
+      this.latitude,
+      this.longitude,
+      this.lastVisit,
+      this.categoryColour});
+}
