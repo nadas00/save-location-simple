3462c8dc3d666a091e5755bd789187d0a6be6e29
hasan çiftçi <hasanciftci@hasan-MacBook-Pro.local>
2020-10-09 21:17:29 +0300

    image_picker implementation for get location image from gallery/camera.

--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -1,9 +1,11 @@
 import 'package:app_settings/app_settings.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:location_permissions/location_permissions.dart';
 import 'package:save_location/db/dao/LocationDao.dart';
 import 'package:save_location/db/database.dart';
+import 'dart:io';
 
 import 'model/location.dart';
 
@@ -44,6 +46,65 @@ class _HomeScreenState extends State<HomeScreen> {
     _long = pLong;
   }
 
+  Future<void> _showChoiceDialog(BuildContext context){
+    return showDialog(context: context, builder: (BuildContext context){
+      return AlertDialog(
+        title: Text('Seçim'),
+        content: SingleChildScrollView(
+          child: ListBody(
+            children: [
+              GestureDetector(
+                child: Text('Galeriye Git'),
+                onTap: (){
+                  _openGallery(context);
+                },
+              ),
+              Padding(padding: EdgeInsets.all(8.0)),
+              GestureDetector(
+                child: Text('Fotoğraf Çek'),
+                onTap: (){
+                  _openCamera(context);
+                },
+              ),
+            ],
+          ),
+        ),
+      );
+    });
+  }
+
+  File imageFile;
+  final picker = ImagePicker();
+
+  _openGallery(BuildContext context) async{
+    var picture = await picker.getImage(source: ImageSource.gallery);
+    this.setState(() {
+      imageFile = File(picture.path);
+    });
+    Navigator.of(context).pop();
+  }
+
+  _openCamera(BuildContext context) async{
+    var picture = await picker.getImage(source: ImageSource.camera);
+    this.setState(() {
+      imageFile = File(picture.path);
+    });
+    Navigator.of(context).pop();
+  }
+
+  Widget _imagePlaceholder(){
+    if(imageFile == null){
+      return Text('Resim Seçilmedi');
+    }else{
+      return Column(
+        children: [
+          Image.file(imageFile,width:MediaQuery.of(context).size.width/2, height:MediaQuery.of(context).size.height/5),
+          Text('Fotoğrafı silmek için çift dokun', style: TextStyle(color: Colors.blueGrey),)
+        ],
+      );
+    }
+  }
+
   @override
   void initState() {
     super.initState();
@@ -58,30 +119,32 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       body: Column(
         children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Form(
-              key: formKey,
-              child: Column(
-                children: <Widget>[
-                  SizedBox(
-                    height: 20,
-                  ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      hoverColor: Colors.blueAccent,
-                      focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.blueAccent)),
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(6.0))),
-                      labelText: 'Konum İsmi',
-                      fillColor: Colors.blueAccent,
-                      contentPadding: EdgeInsets.all(8.0),
-                      hintText: 'Konum ismi giriniz',
+          SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Form(
+                key: formKey,
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(
+                      height: 20,
                     ),
-                    onSaved: (input) => _name = input,
-                  ),
-                ],
+                    TextFormField(
+                      decoration: InputDecoration(
+                        hoverColor: Colors.blueAccent,
+                        focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.blueAccent)),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(6.0))),
+                        labelText: 'Konum İsmi',
+                        fillColor: Colors.blueAccent,
+                        contentPadding: EdgeInsets.all(8.0),
+                        hintText: 'Konum ismi giriniz',
+                      ),
+                      onSaved: (input) => _name = input,
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
@@ -90,86 +153,109 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
           GestureDetector(
             onDoubleTap: () {
-              listLocation.removeLast();
+              setState(() {
+                imageFile = null;
+              });
+
             },
-            child: RaisedButton(
-              color: Colors.blueAccent,
-              child: Text(
-                'Kaydet',
-                style: TextStyle(color: Colors.white),
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal:8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  _imagePlaceholder(),
+                  RaisedButton(
+                    child: Text('Resim Seç',
+                      style: TextStyle(color: Colors.white)),
+                      color: Colors.blue,
+                      onPressed: (){
+                      _showChoiceDialog(context);
+                  }),
+                  SizedBox(height: 10.0,),
+                  RaisedButton(
+                    color: Colors.blueAccent,
+                    child: Text(
+                      'Kaydet',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    onPressed: () async {
+                      ServiceStatus serviceStatus =
+                          await LocationPermissions().checkServiceStatus();
+                      PermissionStatus permissionStatus =
+                          await LocationPermissions().checkPermissionStatus();
+
+                      if (serviceStatus != ServiceStatus.disabled) {
+                        if (permissionStatus != PermissionStatus.denied) {
+                          await getPosition();
+                          formKey.currentState.save();
+                          String lat = _lat ?? 'Konum belirlenemedi';
+                          String long = _long ?? 'Konum belirlenemedi';
+                          if (_name.isEmpty) {
+                            _name = 'Tanımsız Konum';
+                          }
+                          String name = _name;
+
+                          var newLocation =
+                              Location(latitude: lat, longitude: long, name: name);
+                          locationDao.insertLocation(newLocation);
+                          formKey.currentState.reset();
+                        } else {
+                          var locationPermissionDisabledError = AlertDialog(
+                            title: Text('Uygulama izinlerini düzenlemen gerekiyor!'),
+                            content: Text(
+                                'Bu özelliği kullanabilmek için konum izni gereklidir.'),
+                            actions: <Widget>[
+                              FlatButton(
+                                child: Text('Kapat'),
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                              ),
+                              FlatButton(
+                                child: Text('Ayarlar'),
+                                onPressed: () {
+                                  AppSettings.openLocationSettings();
+                                  Navigator.of(context).pop();
+                                },
+                              ),
+                            ],
+                          );
+                          showDialog(
+                              context: context,
+                              builder: (BuildContext context) =>
+                                  locationPermissionDisabledError);
+                        }
+                      } else {
+                        var locationServiceDisabledError = AlertDialog(
+                          title: Text('Konum ayarlarını düzenlemen gerekiyor!'),
+                          content: Text(
+                              'Bu özelliği kullanabilmek için konum servislerini açmanız gerekir.'),
+                          actions: <Widget>[
+                            FlatButton(
+                              child: Text('Kapat'),
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                            FlatButton(
+                              child: Text('Ayarlar'),
+                              onPressed: () {
+                                AppSettings.openLocationSettings();
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                          ],
+                        );
+                        showDialog(
+                            context: context,
+                            builder: (BuildContext context) =>
+                                locationServiceDisabledError);
+                      }
+                    },
+                  ),
+                ],
               ),
-              onPressed: () async {
-                ServiceStatus serviceStatus =
-                    await LocationPermissions().checkServiceStatus();
-                PermissionStatus permissionStatus =
-                    await LocationPermissions().checkPermissionStatus();
-
-                if (serviceStatus != ServiceStatus.disabled) {
-                  if (permissionStatus != PermissionStatus.denied) {
-                    await getPosition();
-                    formKey.currentState.save();
-                    String lat = _lat ?? 'Konum belirlenemedi';
-                    String long = _long ?? 'Konum belirlenemedi';
-                    if (_name.isEmpty) {
-                      _name = 'Tanımsız Konum';
-                    }
-                    String name = _name;
-
-                    var newLocation =
-                        Location(latitude: lat, longitude: long, name: name);
-                    locationDao.insertLocation(newLocation);
-                    formKey.currentState.reset();
-                  } else {
-                    var locationPermissionDisabledError = AlertDialog(
-                      title: Text('Uygulama izinlerini düzenlemen gerekiyor!'),
-                      content: Text(
-                          'Bu özelliği kullanabilmek için konum izni gereklidir.'),
-                      actions: <Widget>[
-                        FlatButton(
-                          child: Text('Kapat'),
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
-                        ),
-                        FlatButton(
-                          child: Text('Ayarlar'),
-                          onPressed: () {
-                            AppSettings.openLocationSettings();
-                          },
-                        ),
-                      ],
-                    );
-                    showDialog(
-                        context: context,
-                        builder: (BuildContext context) =>
-                            locationPermissionDisabledError);
-                  }
-                } else {
-                  var locationServiceDisabledError = AlertDialog(
-                    title: Text('Konum ayarlarını düzenlemen gerekiyor!'),
-                    content: Text(
-                        'Bu özelliği kullanabilmek için konum servislerini açmanız gerekir.'),
-                    actions: <Widget>[
-                      FlatButton(
-                        child: Text('Kapat'),
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                      ),
-                      FlatButton(
-                        child: Text('Ayarlar'),
-                        onPressed: () {
-                          AppSettings.openLocationSettings();
-                        },
-                      ),
-                    ],
-                  );
-                  showDialog(
-                      context: context,
-                      builder: (BuildContext context) =>
-                          locationServiceDisabledError);
-                }
-              },
             ),
           ),
           SizedBox(height: 20),
